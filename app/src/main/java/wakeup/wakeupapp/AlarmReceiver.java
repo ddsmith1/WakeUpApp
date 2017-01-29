7642560f91a0ea0981ea8914e679842340205589
Derek <ddsmith1@mix.wvu.edu>
2017-01-29 01:57:09 -0500

    Added AlarmReceiver.java

---
+++ app/src/main/java/wakeup/wakeupapp/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package wakeup.wakeupapp;
+
+/**
+ * Created by Derek on 1/20/2017.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver
+{
+    static Ringtone ringtone;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+
+    public static void stopMedia() {
+        if(ringtone != null) {
+            ringtone.stop();
+        }
+    }
+}
