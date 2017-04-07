af5af497d8e34ac05b3057d0db66041c4e31ed48
Derek <ddsmith1@mix.wvu.edu>
2017-04-07 19:34:58 -0400

    Updated math game. Some minor modifications, undecided on whether to add in division expressions, but could add very simply. Also, no "ties" among expression answers. Display still mostly unchanged.

--- app/src/main/java/wakeup/wakeupapp/MathGame.java
+++ app/src/main/java/wakeup/wakeupapp/MathGame.java
@@ -20,7 +20,7 @@ public class MathGame extends AppCompatActivity {
     private View.OnClickListener exp3;
     private View.OnClickListener exp4;
 
-    private final int RANGE = 5;
+    private final int RANGE = 3;
     private final int SCORE_NEEDED = 3;
 
     private int median;
@@ -29,9 +29,6 @@ public class MathGame extends AppCompatActivity {
 
     private int expressionFlag;
     private int expAnswers[];
-
-    private int buttonPressed;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,25 +55,15 @@ public class MathGame extends AppCompatActivity {
         score = 0;
         median = 0;
         expressionFlag = 0;
-        buttonPressed = 0;
         random = new Random();
         initAnswers();
         initListeners();
         attachListeners();
     }
 
-    public void attachListeners() {
-        expression1.setOnClickListener(exp1);
-        expression2.setOnClickListener(exp2);
-        expression3.setOnClickListener(exp3);
-        expression4.setOnClickListener(exp4);
-    }
-
     public void initAnswers() {
         expAnswers = new int[4];
-        for(int i = 0; i < 4; i++) {
-            expAnswers[i] = 0;
-        }
+        clearAnswers();
     }
 
     public void initListeners() {
@@ -85,6 +72,7 @@ public class MathGame extends AppCompatActivity {
             public void onClick(View v) {
                 updateScore(checkAnswer(0));
                 checkScore();
+                clearAnswers();
                 gameLoop();
             }
         };
@@ -94,6 +82,7 @@ public class MathGame extends AppCompatActivity {
             public void onClick(View v) {
                 updateScore(checkAnswer(1));
                 checkScore();
+                clearAnswers();
                 gameLoop();
             }
         };
@@ -103,6 +92,7 @@ public class MathGame extends AppCompatActivity {
             public void onClick(View v) {
                 updateScore(checkAnswer(2));
                 checkScore();
+                clearAnswers();
                 gameLoop();
             }
         };
@@ -112,9 +102,17 @@ public class MathGame extends AppCompatActivity {
             public void onClick(View v) {
                 updateScore(checkAnswer(3));
                 checkScore();
+                clearAnswers();
                 gameLoop();
             }
         };
+    }
+
+    public void attachListeners() {
+        expression1.setOnClickListener(exp1);
+        expression2.setOnClickListener(exp2);
+        expression3.setOnClickListener(exp3);
+        expression4.setOnClickListener(exp4);
     }
 
     public void updateScore(boolean value) {
@@ -144,38 +142,38 @@ public class MathGame extends AppCompatActivity {
     }
 
     public void updateButtons() {
-        expressionFlag = 0;
-        expression1.setText(generateExpression());
-        expressionFlag = 1;
-        expression2.setText(generateExpression());
-        expressionFlag = 2;
-        expression3.setText(generateExpression());
-        expressionFlag = 3;
-        expression4.setText(generateExpression());
+            expressionFlag = 0;
+            expression1.setText(generateExpression());
+            expressionFlag = 1;
+            expression2.setText(generateExpression());
+            expressionFlag = 2;
+            expression3.setText(generateExpression());
+            expressionFlag = 3;
+            expression4.setText(generateExpression());
     }
 
     public String generateExpression() {
-        int choice = random.nextInt(2) + 1;
+        String result = null;
+            int choice = random.nextInt(4) + 1;
 
-        String result;
-        switch(choice) {
-            case 1:
-                result = generateAddExpression();
-                break;
-            case 2:
-                int num = random.nextInt(10) + 1 + median - RANGE;
-                expAnswers[expressionFlag] = num;
-                result = Integer.toString(num);
-                break;
-            case 3:
-                result = generateMultExpression();
-                break;
-            case 4:
-                result = generateSubExpression();
-                break;
-            default:
-                result = "0";
-        }
+            switch (choice) {
+                case 1:
+                    result = generateAddExpression();
+                    break;
+                case 2:
+                    int num = random.nextInt(10) + 1 + median - RANGE;
+                    expAnswers[expressionFlag] = num;
+                    result = Integer.toString(num);
+                    break;
+                case 3:
+                    result = generateSubExpression();
+                    break;
+                case 4:
+                    result = generateMultExpression();
+                    break;
+                default:
+                    result = "0";
+            }
         return result;
     }
 
@@ -184,24 +182,57 @@ public class MathGame extends AppCompatActivity {
     }
 
     public String generateSubExpression() {
-        return "0";
+        int operand1 = 0;
+        int operand2 = 0;
+        int difference = 0;
+        while(!(difference >= median - 5 && difference <= median + RANGE )) {
+            operand1 = random.nextInt(median * 2) + 1;
+            operand2 = random.nextInt(median * 2) + 1;
+            difference = operand1 - operand2;
+            for(int i = 0; i < 4; i++) {
+                if(difference == expAnswers[i]) {
+                    difference = -1;
+                }
+            }
+        }
+        expAnswers[expressionFlag] = difference;
+        return operand1 + " - " + operand2;
     }
 
     public String generateMultExpression() {
-        return "0";
+        int operand1 = 0;
+        int operand2 = 0;
+        int product = 0;
+        while(!(product >= median - RANGE && product <= median + RANGE )) {
+            operand1 = random.nextInt(median) + 1;
+            operand2 = random.nextInt(median) + 1;
+            product = operand1 * operand2;
+            for(int i = 0; i < 4; i++) {
+                if(product == expAnswers[i]) {
+                    product = -1;
+                }
+            }
+        }
+        expAnswers[expressionFlag] = product;
+        return operand1 + " x " + operand2;
     }
 
     public String generateAddExpression() {
         int operand1 = 0;
         int operand2 = 0;
         int sum = 0;
-        while(!(sum >= median - 5 && sum <= median + RANGE )) {
-            operand1 = (random.nextInt(10) + 1 + median - RANGE)/2;
-            operand2 = (random.nextInt(10) + 1 + median - RANGE)/2;
+        while(!(sum >= median - RANGE && sum <= median + RANGE )) {
+            operand1 = (random.nextInt(median + RANGE)/2) + 1;
+            operand2 = (random.nextInt(median + RANGE)/2) + 1;
             sum = operand1 + operand2;
+            for(int i = 0; i < 4; i++) {
+                if(sum == expAnswers[i]) {
+                    sum = -1;
+                }
+            }
         }
         expAnswers[expressionFlag] = sum;
-        return operand1 + "+" + operand2;
+        return operand1 + " + " + operand2;
     }
 
     public void checkScore() {
@@ -209,4 +240,11 @@ public class MathGame extends AppCompatActivity {
             finish();
         }
     }
+
+    public void clearAnswers() {
+        for(int i = 0; i < 4; i++) {
+            expAnswers[i] = 0;
+        }
+    }
+
 }
